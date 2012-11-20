a82db70dea3181f953114ba729de2508e0c4c573
Flora Saramago <flora.saramago@gmail.com>
2012-11-19 22:38:59 -0200

    Screen resize feature

--- public/js/script_site.js
+++ public/js/script_site.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
 	$('#code-editor').tabs();
 
 	$('#html-editor').keyup(function () { 
 		$('#simulation').html($('#html-editor').val());
 	});
+
+	$('#resize').on('click', function() {
+		$('#simulation').toggleClass('smaller');
+	});
+	
 });
